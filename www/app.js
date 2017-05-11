36e3d006514fc01472c14cf323b72cf433a60ae4
Lianstu <370845739@qq.com>
2017-05-11 11:53:43 +0800

    引入socket.io完成连接

--- www/app.js
+++ www/app.js
@@ -12,7 +12,19 @@ angular.module('Watermelon',[
   'main.home',
   'main.space'
 ])
-
+  .config(['$ionicAppProvider', '$ionicConfigProvider', function ($ionicAppProvider, $ionicConfigProvider) {
+    $ionicConfigProvider.platform.ios.tabs.style('standard');
+    $ionicConfigProvider.platform.ios.tabs.position('bottom');
+    $ionicConfigProvider.platform.android.tabs.style('standard');
+    $ionicConfigProvider.platform.android.tabs.position('standard');
+    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    $ionicConfigProvider.platform.android.navBar.alignTitle('bottom');//默认为left
+    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+    $ionicConfigProvider.platform.ios.views.transition('ios');
+    $ionicConfigProvider.platform.android.views.transition('android');
+    $ionicConfigProvider.scrolling.jsScrolling(true);
+  }])
   .config(['LoopBackResourceProvider', function (LoopBackResourceProvider) {
     // Change the URL where to access the LoopBack REST API server
     //LoopBackResourceProvider.setUrlBase('https://uclean-api.zhinengxiyifang.cn/api')
--- www/home/controllers/home_chatdetail_ctrl.js
+++ www/home/controllers/home_chatdetail_ctrl.js
@@ -2,9 +2,16 @@
  * Created by lianshaoshuai on 17/4/25.
  */
 angular.module("main.home")
-    .controller("ChatDetailCtrl",["$state",'$scope', 'ngUser', 'MainService', 'User', '$ionicActionSheet','ngHome','$stateParams',ChatDetailCtrl])
+    .controller("ChatDetailCtrl",["$state",'$scope', 'ngUser', 'MainService', 'User', '$ionicActionSheet','ngHome','$stateParams','socket',ChatDetailCtrl])
 
-function ChatDetailCtrl($state,$scope, ngUser, MainService, User, $ionicActionSheet,ngHome,$stateParams){
+function ChatDetailCtrl($state,$scope, ngUser, MainService, User, $ionicActionSheet,ngHome,$stateParams,socket){
     console.log("ChatCtrl")
-    $scope.chat = ngHome.getchatbyid($stateParams.chatId);
+    $scope.messageDetils = ngHome.getchatbyid($stateParams.chatId);
+    console.log("chat",$scope.messageDetils)
+    var self = this;
+    self.sendmessage = sendmessage;
+    function  sendmessage(){
+        console.log("send message",$scope.send_content)
+        $scope.send_content = "";
+    }
 }
